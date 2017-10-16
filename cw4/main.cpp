c8ce05219f5525cf6fc3b90eaba9144e3de1cff7
SokolowskiKrzysztof <krzych1257@interia.pl>
2017-10-16 17:21:42 +0200

    prawie dziala

--- cw4/main.cpp
+++ cw4/main.cpp
@@ -36,7 +36,7 @@ vector <Probka> wczytaj (string n)
 void zapis(vector <Probka> dane, string nazwa)
 {
    ofstream plik (nazwa);
-   plik << "Bartosz Szwed ";
+
    for (int i=0;i<dane.size();i++)
    {
        plik<<dane[i].t <<", "<<dane[i].x<< endl;
@@ -44,17 +44,52 @@ void zapis(vector <Probka> dane, string nazwa)
    plik.close();
 }
 
+double obliczDlugosc (vector<Probka> jjj)
+{
+    double e=1000000;
+    double a=-1000000;
 
+        double d;
+    for(int i=0;i<jjj.size();i++)
+
+    {
+
+        if(jjj[i].x>a)
+        {
+
+
+            a=jjj[i].x;
+            }
+
+        if(jjj[i].x<e)
+        {
+
+
+        e=jjj[i].x;
+        }
+
+    }
+
+    cout<<endl;
+    cout<<a<<endl;
+    cout<<e<<endl;
+    d=a-e;
+    cout<<"dlugosc wynosi"<<d<<endl;
+    cout<<jjj.size()<<endl;
+    return d;
+}
 int main(int argc,char* argv[])
 {
 
    if (argc != 2) {
  return -1;
 }
-vector <Probka> dd=wczytaj(argv [1]);
+vector <Probka> kk=wczytaj(argv [1]);
+obliczDlugosc(kk);
 string nazwa_pliku = argv[1];
 cout << "Odczytuje plik: " << nazwa_pliku << endl;
-zapis (dd, "nazwa.csv");
+
+zapis (kk, "nazwa.csv");
     cin.get();
     return 0;
 }
